62635f22760e1b178e8aa9bb751626dab8ec9106
Aldwoni (Pim) <aldwoni@gmail.com>
2017-01-20 23:09:28 +0100

    switching methode

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-
+    public Character currentCharacter;
 	// Use this for initialization
 	void Start () {
 		
---
+++ Assets/Scripts/SwitchingCharacter.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class SwitchingCharacter : MonoBehaviour {
+    int playercount;
+    List<Character> characters = new List<Character>();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Switch(Player p)
+    {
+        int newCharacterindex = p.currentCharacter.index + playercount;
+        Character newCharacter = characters[newCharacterindex];
+        p.currentCharacter = newCharacter;    
+    }
+}
