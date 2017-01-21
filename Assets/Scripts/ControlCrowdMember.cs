2fb94a5d0fbfeddc0fa4bb5139c9844172932398
koenraadkragt <koenraad.kragt@gmail.com>
2017-01-21 10:51:53 +0100

    Crowd Character arm controlling and jumping

--- Assets/Scripts/ControlCrowdMember.cs
+++ Assets/Scripts/ControlCrowdMember.cs
@@ -11,47 +11,73 @@ public class ControlCrowdMember : MonoBehaviour {
     [SerializeField]
     private Transform leftArmPivot;
     [SerializeField]
-    private Vector2 jumpHeight;
+    private Vector2 jumpVelocity;
+    [SerializeField]
+    private float jumpDuration;
 
     private bool grounded = true;
 
-    public void moveRightArm(float inputRightArm)
+    const float axisMargin = 0.5f;
+
+    public void moveRightArm(Vector2 rightArmInput)
     {
-        rightArmPivot.Rotate(0, 0, -inputRightArm * rotateSpeed);
+        if(rightArmInput.magnitude > axisMargin)
+        {
+            rightArmInput = rightArmInput.normalized;
+            Quaternion targetRotation = Quaternion.FromToRotation(Vector2.right, rightArmInput);
+            rightArmPivot.transform.rotation = targetRotation;
+        }
     }
 
-    public void moveLeftArm(float inputLeftArm)
+    public void moveLeftArm(Vector2 leftArmInput)
     {
-        leftArmPivot.Rotate(0, 0, -inputLeftArm * rotateSpeed);
+        if (leftArmInput.magnitude > axisMargin)
+        {
+            leftArmInput = leftArmInput.normalized;
+            Quaternion targetRotation = Quaternion.FromToRotation(Vector2.right, leftArmInput);
+            leftArmPivot.transform.rotation = targetRotation;
+        }
     }
 
     public void jump()
     {
-        if (!grounded)
+        if (grounded)
         {
-            return;
+            grounded = false;
+            StartCoroutine(jumpRoutine());
         }
-        grounded = false;
-        GetComponent<Rigidbody2D>().AddForce(jumpHeight, ForceMode2D.Impulse);
+    }
+
+    private IEnumerator jumpRoutine()
+    {
+
+        GetComponent<Rigidbody2D>().velocity = jumpVelocity;
+        yield return new WaitForSeconds(jumpDuration);
+
+        GetComponent<Rigidbody2D>().velocity = -jumpVelocity;
     }
 
     //temporary: actual input should be done by player
     void Update()
     {
-        moveRightArm(Input.GetAxisRaw("Horizontal"));
-        moveLeftArm(Input.GetAxisRaw("Vertical"));
-
+        moveRightArm(new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxis("Vertical")));
+        moveLeftArm(new Vector2(-Input.GetAxisRaw("Joystick1AnalogRightX"), Input.GetAxis("Joystick1AnalogRightY")));
+        
         if (Input.GetButtonDown("Jump"))
         {
-            Debug.Log("Jump");
+            Debug.Log("jump");
             jump();
         }
     }
 
-    void OnCollisionEnter2D(Collision2D coll)
+    void FixedUpdate()
     {
-        if (coll.gameObject.tag == "Ground")
+        if (transform.position.y < 0)
         {
+            Debug.Log("landed");
+            transform.position = new Vector3(transform.position.x, 0, transform.position.z);
+
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
             grounded = true;
         }
     }
