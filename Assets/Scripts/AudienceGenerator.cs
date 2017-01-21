b8d1c9d8541bb45a8a23e52263c474931ad3fb00
Aldwoni (Pim) <aldwoni@gmail.com>
2017-01-21 10:42:28 +0100

    distance tussen characters beter aan te passen

--- Assets/Scripts/AudienceGenerator.cs
+++ Assets/Scripts/AudienceGenerator.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AudienceGenerator : MonoBehaviour {
-    public int distanceBetweenCharacters;
+    public float distanceBetweenCharacters;
     public Character characterPrefab;
     public string characterName;
     public int AmountOfCharactersToCreateAtStart;
