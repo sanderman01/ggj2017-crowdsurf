1ed8fef710b153949f2e6ac73a9717a3b407f80c
Alexander Verbeek <sanderman@gmail.com>
2017-01-21 16:43:18 +0100

    Add a basic start on Stats tracking functionality.

---
+++ Assets/Scripts/Stats.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Manages tracking of statistics.
+/// </summary>
+public static class Stats {
+
+    public enum GameEndedReason { Finished, Failed, Reset }
+
+    public struct GlobalStats {
+        private int total_games;
+        private int total_games_finished;
+        private int total_games_failed;
+        private int total_games_reset;
+
+        private float best_duration;
+
+        private int jumps_total;
+        private float jumps_longest_airtime;
+        private float jumps_highest;
+
+        // TODO More stats here...
+
+        /// <summary>
+        /// Apply the last game's stats to the global stats, to update counters and records.
+        /// </summary>
+        public static GlobalStats ApplyStats(GlobalStats globals, GameStats game) {
+
+            globals.total_games += 1;
+            switch (game.game_end_reason) {
+                case GameEndedReason.Finished:
+                    globals.total_games_finished += 1;
+                    break;
+                case GameEndedReason.Failed:
+                    globals.total_games_failed += 1;
+                    break;
+                case GameEndedReason.Reset:
+                    globals.total_games_reset += 1;
+                    break;
+            }
+
+            globals.best_duration = Mathf.Max(globals.best_duration, game.game_duration);
+            globals.jumps_total += game.jumps_total;
+            globals.jumps_longest_airtime = Mathf.Max(globals.jumps_longest_airtime, game.jumps_longest_airtime);
+            globals.jumps_highest = Mathf.Max(globals.jumps_highest, game.jumps_highest);
+
+            // TODO More stats here...
+
+            return globals;
+        }
+    }
+
+    public struct GameStats {
+        public float game_begin;
+        public float game_end;
+        public float game_duration;
+        public GameEndedReason game_end_reason;
+
+        public int jumps_total;
+        public float jumps_longest_airtime;
+        public float jumps_highest;
+
+        // TODO More stats here...
+    }
+
+    private static GlobalStats globals = new GlobalStats(); // maybe save/load from file in future?
+    private static GameStats currentGame;
+    private static GameStats previousGame;
+
+    public static void StartGame() {
+        previousGame = currentGame;
+        currentGame = new GameStats();
+
+        currentGame.game_begin = Time.time;
+    }
+
+    public static void Update() {
+        currentGame.game_end = Time.time;
+        currentGame.game_duration = currentGame.game_end - currentGame.game_begin;
+    }
+
+    public static void EndGame(GameEndedReason reason) {
+        currentGame.game_end = Time.time;
+        currentGame.game_duration = currentGame.game_end - currentGame.game_begin;
+        currentGame.game_end_reason = reason;
+
+        globals = GlobalStats.ApplyStats(globals, currentGame);
+    }
+}
