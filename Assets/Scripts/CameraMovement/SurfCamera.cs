fd29ec5d53b6438301cfe41911d055b3863aa0b4
Alexander Verbeek <sanderman@gmail.com>
2017-01-21 15:00:59 +0100

    Added SurfCamera. This camera was designed to follow the surfer and expand to show both the surfer and the characters at all times.

---
+++ Assets/Scripts/CameraMovement/SurfCamera.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This camera needs to satisfy the following constraints with smooth movement:
+/// Keep all player controlled characters in view.
+/// Keep the surfer near the center of the screen.
+/// This was designed for an orthographic camera.
+/// </summary>
+public class SurfCamera : MonoBehaviour {
+
+    [SerializeField]
+    private float period = 0.4f;
+
+    [SerializeField]
+    private float minCameraSize = 1.5f;
+
+    [SerializeField]
+    private float floorY = -0.1f;
+
+    private Camera cam;
+    private Surfer surfer;
+    new private Rigidbody2D rigidbody;
+
+    void Awake() {
+        cam = GetComponent<Camera>();
+        rigidbody = GetComponent<Rigidbody2D>();
+        surfer = GameObject.FindObjectOfType<Surfer>();
+    }
+
+    void Start() {
+    }
+
+    void Update() {
+        // Steer towards surfer.
+        Vector3 targetPosition = surfer.AveragePosition * 0.75f;
+        Vector3 targetDirection = targetPosition - transform.position;
+        float distance = targetDirection.magnitude;
+        float speed = distance / period;
+        rigidbody.velocity = targetDirection.normalized * speed;
+
+        // Expand size when neccesary
+        cam.orthographicSize = Mathf.Max(minCameraSize, transform.position.y - floorY);
+    }
+}
