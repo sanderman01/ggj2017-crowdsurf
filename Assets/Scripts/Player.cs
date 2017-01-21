2e7d374d629c8ea81cb0ddf44e645ac14e15e0ce
Mark de Wit <mark_de_wit_95@hotmail.com>
2017-01-21 11:27:41 +0100

    Added scene with merged files.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,13 +6,20 @@ public class Player : MonoBehaviour {
     public int playerNumber;
     public Color color;
     public Character currentCharacter;
+
+    Controls controls = new Controls();
+    public Shader shader;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        controls.SetJoystickID(playerNumber);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (controls.ADown())
+        {
+            this.gameObject.GetComponent<SwitchingCharacter>().Switch(this);
+        }
 	}
 }
