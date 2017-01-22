d1ddc5e0880c71ecfdba89c1337e264a158084b3
koenraadkragt <koenraad.kragt@gmail.com>
2017-01-22 16:15:39 +0100

    crowd cheer

---
+++ Assets/Scripts/CrowdCheer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrowdCheer : MonoBehaviour {
+
+    public Surfer surfer;
+    public float cheerHeight = 2.5f;
+
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (surfer.MaxHeight > cheerHeight && !audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(audioSource.clip);
+        }
+    }
+}
