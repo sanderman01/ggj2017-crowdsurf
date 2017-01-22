4e61049bcfe7cb33d0a714aab3a61516c1a631ee
Aldwoni (Pim) <aldwoni@gmail.com>
2017-01-22 02:00:58 +0100

    first character skins

--- Assets/Scripts/AudienceGenerator.cs
+++ Assets/Scripts/AudienceGenerator.cs
@@ -7,6 +7,8 @@ public class AudienceGenerator : MonoBehaviour {
     public Character characterPrefab;
     public string characterName;
     public int AmountOfCharactersToCreateAtStart = 4;
+    public List<CharacterSkin> skins;
+    public int count;
 
 	// Use this for initialization
 	void Start () {
@@ -32,6 +34,7 @@ public class AudienceGenerator : MonoBehaviour {
 
     public void GenerateNewCharacter()
     {
+        count++;
         Vector2 newLocation;
         if(Audience.characters.Count == 0)
         {
@@ -43,7 +46,30 @@ public class AudienceGenerator : MonoBehaviour {
         }
         Character newCharacter = Instantiate(characterPrefab, new Vector3(newLocation.x,newLocation.y),Quaternion.identity);
         Audience.characters.Add(newCharacter);
+        if(count % 2 == 0)
+        {
+            AddSkin(newCharacter);
+        }
+        
         newCharacter.gameObject.name = string.Concat(characterName, Audience.characters.Count);
         newCharacter.index = Audience.characters.Count-1;
+    }
+    public void AddSkin(Character c)
+    {
+        var shoulderRight = c.transform.Find("Shoulder_right/Arm/sprite");
+        var shoulderLeft = c.transform.Find("Shoulder_left/Arm/sprite");
+        var legs = c.transform.Find("Body/legs/sprite");
+        var torso = c.transform.Find("Body/torso/sprite");
+        var head = c.transform.Find("Body/Head/sprite");
+        var sp1 = head.GetComponent<SpriteRenderer>();
+        var sp2 = torso.GetComponent<SpriteRenderer>();
+        var sp3 = legs.GetComponent<SpriteRenderer>();
+        var sp4 = shoulderRight.GetComponent<SpriteRenderer>();
+        var sp5 = shoulderLeft.GetComponent<SpriteRenderer>();
+        sp1.sprite = skins[0].head;
+        sp2.sprite = skins[0].Body;
+        sp3.sprite = skins[0].Legs;
+        sp4.sprite = skins[0].Arm;
+        sp5.sprite = skins[0].Arm;
     }
 }
---
+++ Assets/Scripts/CharacterSkin.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class CharacterSkin : MonoBehaviour {
+    public Sprite head;
+    public Sprite Arm;
+    public Sprite Legs;
+    public Sprite Body;
+}
